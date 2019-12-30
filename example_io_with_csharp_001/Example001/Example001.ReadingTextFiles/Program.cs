cb2dac55d9b65f39af7d88ff2d12ce29a32f2440
flaviogf <flavio.fernandes6@gmail.com>
2019-12-30 14:59:41 -0300

    .

---
+++ example_io_with_csharp_001/Example001/Example001.ReadingTextFiles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Example001.ReadingTextFiles
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using var file = new FileStream("contas.txt", FileMode.Open, FileAccess.Read);
+
+            var encoding = Encoding.UTF8;
+
+            var buffer = new byte[1024];
+
+            var bytesReaded = -1;
+
+            while ((bytesReaded = file.Read(buffer, 0, 1024)) != 0)
+            {
+                Console.WriteLine(encoding.GetString(buffer, 0, bytesReaded));
+            }
+        }
+    }
+}
