d255d005c2e06583e394895224508f752ead902d
flaviogf <flavio.fernandes6@gmail.com>
2019-12-31 14:39:39 -0300

    .

---
+++ example_io_with_csharp_001/Example005/Example005.ConsoleStream/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Example005.ConsoleStream
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var stream = Console.OpenStandardInput())
+            using (var file = new FileStream("output.txt", FileMode.Create, FileAccess.Write))
+            {
+                while(true)
+                {
+                    var buffer = new byte[1024];
+
+                    var readed = stream.Read(buffer, 0, 1024);
+
+                    file.Write(buffer, 0, readed);
+
+                    file.Flush();
+
+                    Console.WriteLine($"Bytes readed {readed}");
+                }
+            }
+        }
+    }
+}
