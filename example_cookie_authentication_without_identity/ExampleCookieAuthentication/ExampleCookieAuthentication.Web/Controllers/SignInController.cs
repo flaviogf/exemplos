729fecc218ec941063178f3c5f0b12cc45063efc
flaviogf <flavio.fernandes6@gmail.com>
2020-02-08 11:29:52 -0300

    csharp: example_cookie_authentication_without_identity

---
+++ example_cookie_authentication_without_identity/ExampleCookieAuthentication/ExampleCookieAuthentication.Web/Controllers/SignInController.cs
@@ -0,0 +1,36 @@
+﻿using ExampleCookieAuthentication.Web.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace ExampleCookieAuthentication.Web.Controllers
+{
+    public class SignInController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(SignInViewModel viewModel)
+        {
+            var claims = new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "Flavio")
+            };
+
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var properties = new AuthenticationProperties();
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity), properties);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ example_cookie_authentication_without_identity/ExampleCookieAuthentication/ExampleCookieAuthentication.Web/Startup.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ExampleCookieAuthentication.Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                    .AddCookie(it =>
+                    {
+                        it.LoginPath = "/SignIn/Index";
+                    });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(it =>
+            {
+                it.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
