a6e358d20b6e965638ed80b7959ba496ffb879d2
flaviogf <flavio.fernandes6@gmail.com>
2020-02-29 21:23:22 -0300

    azure: storage queue

---
+++ example_azure_storage_queue/Example/Example.Receiver/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Queue;
+using System;
+
+namespace Example.Receiver
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var connectionString = "";
+
+            var account = CloudStorageAccount.Parse(connectionString);
+
+            var client = account.CreateCloudQueueClient();
+
+            var queue = client.GetQueueReference("myqueue");
+
+            CloudQueueMessage message;
+
+            while ((message = queue.GetMessage()) != null)
+            {
+                Console.WriteLine("Message was received.");
+                Console.WriteLine(message.AsString);
+
+                queue.DeleteMessage(message);
+            }
+        }
+    }
+}
---
+++ example_azure_storage_queue/Example/Example.Sender/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Queue;
+using System.Linq;
+
+namespace Example.Sender
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var connectionString = "";
+
+            var account = CloudStorageAccount.Parse(connectionString);
+
+            var client = account.CreateCloudQueueClient();
+
+            var queue = client.GetQueueReference("myqueue");
+
+            foreach (var number in Enumerable.Range(0, 5))
+            {
+                var message = new CloudQueueMessage($"Message nº {number}");
+
+                queue.AddMessage(message);
+            }
+        }
+    }
+}
