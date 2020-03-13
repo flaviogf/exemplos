df3b55f683aa93045c3e144d22fa1a229f0771f8
flaviogf <flavio.fernandes6@gmail.com>
2020-03-13 16:37:06 -0300

    example: asp.net core background service

---
+++ example_background_service_asp_net_core/Example/Example.BackgroundTask/DoSomethingHostedService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Example.BackgroundTask
+{
+    public class DoSomethingHostedService : IHostedService
+    {
+        private readonly ILogger<DoSomethingHostedService> _logger;
+
+        public DoSomethingHostedService(ILogger<DoSomethingHostedService> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Start doing something");
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Stop doing something");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ example_background_service_asp_net_core/Example/Example.BackgroundTask/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Example.BackgroundTask
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder().ConfigureServices(it => it.AddHostedService<DoSomethingHostedService>());
+    }
+}
