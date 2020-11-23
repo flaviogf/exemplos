ac05bd9cbe875a5b4eb1967cce3d7f3576f5c4c0
flaviogf <flavio.fernandes6@gmail.com>
2020-11-23 19:56:00 -0300

    new csharp features

--- timcorey_new_csharp_features/src/NewCsharpFeatures/Person.cs
+++ timcorey_new_csharp_features/src/NewCsharpFeatures/Person.cs
@@ -2,10 +2,23 @@ namespace NewCsharpFeatures
 {
     public class Person
     {
+        public Person(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public Person()
+        {
+        }
+
         public int Id { get; init; }
 
         public string FirstName { get; init; }
 
         public string LastName { get; init; }
+
+        public override string ToString() => $"Person[Id={Id}, FirstName={FirstName}, LastName={LastName}]";
     }
 }
--- timcorey_new_csharp_features/src/NewCsharpFeatures/Program.cs
+++ timcorey_new_csharp_features/src/NewCsharpFeatures/Program.cs
@@ -7,7 +7,13 @@ Console.WriteLine(Add(10, 10));
 
 var frank = new Person { Id = 1, FirstName = "Frank", LastName = "Fernandes" };
 
+Console.WriteLine(frank);
+
 // frank.FirstName = "Something"; doesn't work
+
+Person nina = new(id: 2, firstName: "Nina", lastName: "Fernandes");
+
+Console.WriteLine(nina);
 
 double Add(double x, double y)
 {
