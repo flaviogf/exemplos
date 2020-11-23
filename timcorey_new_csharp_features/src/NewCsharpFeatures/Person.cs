412ee4a758bce7bff0804b9970d20bb76e69a487
flaviogf <flavio.fernandes6@gmail.com>
2020-11-23 19:47:28 -0300

    new csharp features

---
+++ timcorey_new_csharp_features/src/NewCsharpFeatures/Person.cs
@@ -0,0 +1,11 @@
+namespace NewCsharpFeatures
+{
+    public class Person
+    {
+        public int Id { get; init; }
+
+        public string FirstName { get; init; }
+
+        public string LastName { get; init; }
+    }
+}
--- timcorey_new_csharp_features/src/NewCsharpFeatures/Program.cs
+++ timcorey_new_csharp_features/src/NewCsharpFeatures/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using NewCsharpFeatures;
 
 Console.WriteLine("Hello World");
 
 Console.WriteLine(Add(10, 10));
+
+var frank = new Person { Id = 1, FirstName = "Frank", LastName = "Fernandes" };
+
+// frank.FirstName = "Something"; doesn't work
 
 double Add(double x, double y)
 {
