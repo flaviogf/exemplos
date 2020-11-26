59b050e206bc7a7e181e8f16c00cd8a9389b0bf0
flaviogf <flavio.fernandes6@gmail.com>
2020-11-26 20:30:13 -0300

    new csharp features

---
+++ timcorey_intro_to_records/src/RecordDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RecordDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public record Record1(string FirstName, string LastName);
+}
