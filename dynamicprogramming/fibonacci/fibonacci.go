1e894b9a6391bcaf17d3fd8e71fbfa66d139b820
flaviogf <flavio.fernandes6@gmail.com>
2021-04-14 09:02:13 -0300

    dynamic programming

---
+++ dynamicprogramming/fibonacci/fibonacci.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	n := 25
+
+	start := time.Now()
+
+	fmt.Println(fib(n))
+
+	fmt.Println(time.Since(start).Nanoseconds()) // 1097500
+
+	start = time.Now()
+
+	fmt.Println(fibMemo(n, make([]int, n+1)))
+
+	fmt.Println(time.Since(start).Nanoseconds()) // 1064900
+
+	start = time.Now()
+
+	fmt.Println(fibMemoWithoutRecursion(n))
+
+	fmt.Println(time.Since(start).Nanoseconds()) // 0
+}
+
+func fib(n int) int {
+	if n == 1 || n == 2 {
+		return 1
+	}
+
+	return fib(n-1) + fib(n-2)
+}
+
+func fibMemo(n int, memo []int) int {
+	if n == 1 || n == 2 {
+		return 1
+	}
+
+	if memo[n] != 0 {
+		return memo[n]
+	}
+
+	memo[n] = fib(n-1) + fib(n-2)
+
+	return memo[n]
+}
+
+func fibMemoWithoutRecursion(n int) int {
+	if n == 1 || n == 2 {
+		return 1
+	}
+
+	memo := make([]int, n+1)
+
+	memo[1] = 1
+
+	memo[2] = 1
+
+	for i := 3; i < n+1; i++ {
+		memo[i] = memo[i-1] + memo[i-2]
+	}
+
+	return memo[n]
+}
---
+++ dynamicprogramming/go.mod
@@ -0,0 +1,3 @@
+module github.com/flaviogf/dynamicprogramming
+
+go 1.16
