955dc9a50b837c0ebb2909a6530023a50321b783
flaviogf <flavio.fernandes6@gmail.com>
2022-08-15 14:04:21 -0300

    ruby_2d_build_a_simple_2d_pong_game

--- ruby_2d_build_a_simple_2d_pong_game/game.rb
+++ ruby_2d_build_a_simple_2d_pong_game/game.rb
@@ -2,4 +2,33 @@
 
 require 'ruby2d'
 
+class Paddle
+  def initialize(position:)
+    @position = position
+
+    @width = 8
+    @height = 45
+
+    @x = if position == :right
+           Window.width - @width
+         else
+           0
+         end
+
+    @y = Window.height / 2 - @height
+  end
+
+  def draw
+    Rectangle.new(x: @x, y: @y, width: @width, height: @height)
+  end
+end
+
+player1 = Paddle.new(position: :left)
+player2 = Paddle.new(position: :right)
+
+update do
+  player1.draw
+  player2.draw
+end
+
 show
