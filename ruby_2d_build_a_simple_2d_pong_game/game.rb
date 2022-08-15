8a5619d815c705ec7b1a48a9d5f942252acc5528
flaviogf <flavio.fernandes6@gmail.com>
2022-08-15 14:16:18 -0300

    ruby_2d_build_a_simple_2d_pong_game

--- ruby_2d_build_a_simple_2d_pong_game/game.rb
+++ ruby_2d_build_a_simple_2d_pong_game/game.rb
@@ -3,6 +3,8 @@
 require 'ruby2d'
 
 class Paddle
+  attr_accessor :direction
+
   def initialize(position:)
     @position = position
 
@@ -16,19 +18,50 @@ class Paddle
          end
 
     @y = Window.height / 2 - @height
+
+    @velocity = 5
   end
 
   def draw
     Rectangle.new(x: @x, y: @y, width: @width, height: @height)
+  end
+
+  def move
+    case direction
+    when :down
+      @y += @velocity
+    when :up
+      @y -= @velocity
+    end
   end
 end
 
 player1 = Paddle.new(position: :left)
 player2 = Paddle.new(position: :right)
 
+on :key_held do |event|
+  case event.key
+  when 'j'
+    player1.direction = :down
+  when 'k'
+    player1.direction = :up
+  end
+end
+
+on :key_up do |event|
+  case event.key
+  when 'j', 'k'
+    player1.direction = nil
+  end
+end
+
 update do
+  clear
+
   player1.draw
   player2.draw
+
+  player1.move
 end
 
 show
