f3bf9f4f32d9094dae2ab5dc1f23311f6b3b1089
flaviogf <flavio.fernandes6@gmail.com>
2022-08-15 13:55:16 -0300

    ruby_2d_build_a_simple_2d_pong_game

--- ruby_2d_build_a_simple_2d_pong_game/game.rb
+++ ruby_2d_build_a_simple_2d_pong_game/game.rb
@@ -2,40 +2,4 @@
 
 require 'ruby2d'
 
-class Paddle
-  attr_reader :position, :width, :height
-
-  def initialize(position: :left, width: 8, height: 20)
-    @position = position
-    @width = width
-    @height = height
-  end
-
-  def draw
-    Rectangle.new(x: x, y: y, width: width, height: height)
-  end
-
-  private
-
-  def x
-    return Window.width - width if position == :right
-
-    0
-  end
-
-  def y
-    @y = Window.height / 2 - height
-  end
-end
-
-player1 = Paddle.new
-player2 = Paddle.new(position: :right)
-
-update do
-  player1.draw
-  player2.draw
-end
-
-set width: 640, height: 480
-
 show
